86e991cd744e795fc4170ad1e9c319de9e759722
Rodrigo Novais <irodrigoan@gmail.com>
2024-06-15 01:23:10 -0300

    Aula 01 - Variáveis

---
+++ aula-01.js
@@ -0,0 +1,19 @@
+let idade;
+idade = 25;
+let nome = 'Rodrigo', sobrenome = 'Novais', telefone = '99999999';
+console.log(nome, sobrenome, telefone)
+
+// let - Ele permite alterar o seu valor
+// const - Ele possui um valor inicial, porem não permite alterar seu valor
+// var - É antigo e não é mais recomendado utilizar devido seu comportamento imprevisível
+
+let quantidade = 5;
+const preco = 10;
+
+quantidade = 7
+console.log(quantidade)
+preco = 15
+console.log(preco)
+
+const x = 80
+const peso = 80
