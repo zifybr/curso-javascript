d85dcdf1d99fceecfe76782ddbb11074f276ca3b
Rodrigo Novais <irodrigoan@gmail.com>
2024-06-15 19:35:54 -0300

    Aula 03

---
+++ aula-03.js
@@ -0,0 +1,21 @@
+// Números
+
+// let idade1 = 25;
+// let 1idade = 30;
+
+// Palavras reservadas
+
+// let const = 30;
+
+// Caracteres especiais
+
+// let $idade = 30;
+// let _idade = 40;
+// let idade do fulano = 50;
+// console.log($idade,_idade)
+
+// Maiúscula e minúscula
+
+let Idade = 30;
+let idade = 40;
+console.log(Idade, idade)
