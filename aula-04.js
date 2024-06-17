6f0aee2733d5fa10bac1e5aae6e186935321c9ee
Zify <cadastro@zify.com.br>
2024-06-16 21:17:29 -0300

    Aula 04

---
+++ aula-04.js
@@ -0,0 +1,44 @@
+// Number
+
+let idade = 25
+let preco = 20.99
+
+// String
+
+let nome = 'Rodrigo'
+let saudacao = "Olá, bem vindo"
+let outraSaudacao = `Obrigado por estar aqui`
+
+// Boolean
+
+let estouEstudando = true
+let estaChovendo = false
+
+// Undefined
+
+let idade1;
+// console.log(idade1)
+
+// Null
+let idade2 = null
+// console.log(idade2)
+
+// Symbol
+
+let nome1 = Symbol('Rodrigo')
+let nome2 = Symbol('Rodrigo')
+
+// console.log(nome1 === nome2)
+
+// BigInt
+
+let numero = 123n
+// console.log(numero)
+
+console.log(typeof 1)
+console.log(typeof "string")
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof Symbol('Rodrigo'))
+console.log(typeof 123n)
