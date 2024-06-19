8434e3cc8092664517bd040b41bd08e920a13638
Zify <cadastro@zify.com.br>
2024-06-18 22:41:33 -0300

    Aula 06

---
+++ aula-06.js
@@ -0,0 +1,26 @@
+// Operadores Aritméticos
+
+let soma = 5 + 1 // Adição (+)
+let subtracao = 5 - 1 // Subtração (-)
+let multiplicacao = 5 * 2 // Multiplicação (*)
+let divisao = 10 / 2 // Divisão (/)
+let modulo = 12 % 3 // Módulo (%)
+let exponenciacao = 10 ** 2 // Exponenciação (**)
+
+let incremento = 5; // Incremento
+incremento++;
+
+let decremento = 5; // Decremento
+decremento--;
+
+
+// Operador de atribuição
+
+let a = 10
+
+a += 5 // (a + 5) = 15
+a -= 5 // (a - 5) = 10
+a *= 10 // (a * 10) = 100
+a /= 10 // (a / 10) = 10
+a %= 10 // (a % 10) = 0
+a **= 3 // (a ** 3) = 0 
