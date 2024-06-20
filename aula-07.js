0390e179fb422f8954904fd5f4625f20b37764a3
Zify <cadastro@zify.com.br>
2024-06-19 22:28:59 -0300

    Aula 07

---
+++ aula-07.js
@@ -0,0 +1,21 @@
+// Operadores de comparação
+
+console.log(5 == 4) // Igual (==) -> Verifica se o valor é igual
+console.log(5 != 4) // Diferente ou não igual (!=) -> Verifica se o valor é diferente ou nào igual
+console.log(5 === '5') // Estritamente igual (===) -> Verifica se o valor e o tipo são iguais
+console.log(5 !== '5') // Estritamente não igual (!==) -> Verifica se o valor ou o tipo é diferente
+console.log(6 > 5) // Maior (>) -> Verifica se o valor da esquerda é maior que o da direita
+console.log(5 < 6) // Menor (<) -> Verifica se o valor da esquerda é menor que o da direita
+console.log(5 >= 5) // Maior ou igual (>=) -> Verifica se o valor da esquerda é maior ou igual o da direita
+console.log(5 <= 4) // Menor ou giual (<=) -> Verifica se o valor da esquerda é menor ou igual o da direita
+
+// Operadores lógicos
+
+let x = true
+let y = true
+let z = false
+
+console.log(x && z) // E lógico (&&) -> Retorna true caso todos os operandos sejam verdadeiros
+console.log(x || z) // Ou lógico (||) -> Retorna true caso um dos operandos sejam verdadeiros
+console.log(!x) // Não lógico (!) -> Converte qualquer valor para o seu oposto booleano
+console.log(!z) // 'Igual ao de cima'
