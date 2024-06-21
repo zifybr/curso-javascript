3e2dd970d7d3786a70e058e3b46a3e6ba2963dee
Zify <cadastro@zify.com.br>
2024-06-20 22:02:01 -0300

    Aula 08

---
+++ aula-08.js
@@ -0,0 +1,14 @@
+// if - Se
+// else - caso contrário
+
+let nota = 69
+
+if (nota >= 90) {
+  console.log('A')
+} else if (nota >= 80) {
+  console.log('B')
+} else if (nota >= 70) {
+  console.log('C')
+} else {
+  console.log('D')
+}
