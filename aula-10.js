9e0dd595b3dd375691123e55639f3d0fdb1920e6
Zify <cadastro@zify.com.br>
2024-06-23 20:31:54 -0300

    Aula 10

---
+++ aula-10.js
@@ -0,0 +1,10 @@
+// Operador de Coalescência nula
+
+let saudacao = 'Seja bem vindo ao nosso curso de JavaScript'
+let mensagem = saudacao ?? 'Seja bem vindo'
+
+// Operador Ternário
+
+const idade = 16
+let maiorDeIdade = idade >= 18 ? 'maior de idade' : 'menor de idade'
+console.log(maiorDeIdade)
