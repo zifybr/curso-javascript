0e79d38b1a2745d1f6e77e707a23da5eb2236674
Zify <cadastro@zify.com.br>
2024-06-27 23:45:15 -0300

    Aula 15

---
+++ aula-15.js
@@ -0,0 +1,44 @@
+// function
+
+// function nomeDaFuncao() {
+//   console.log('Estou aprendendo javascript na aula 15')
+//   // return 'Estou aprendendo javascript na aula 15'
+// }
+
+// nomeDaFuncao()
+// console.log(nomeDaFuncao())
+
+// function saudacao(nome) {
+//   return 'Seja bem vindo ' + nome
+// }
+
+// console.log(saudacao('Rodrigo'))
+
+// function soma(numero1, numero2) {
+//   return numero1 + numero2
+// }
+
+// console.log(soma(40, 20))
+
+function parOuImpar(numero) {
+  if (numero % 2 === 0) {
+    return 'Par'
+  } else {
+    return 'Impar'
+  }
+}
+
+console.log(parOuImpar(2))
+console.log(parOuImpar(4))
+console.log(parOuImpar(7))
+
+// console.log(parOuImpar(9))
+
+// const exemplo = function () {
+//   console.log('Olá')
+// }
+
+
+// const exemplo1 = (nome) => 'Bem vindo ' + nome
+
+// console.log(exemplo1('Rodrigo'))
