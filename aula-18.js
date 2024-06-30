77e3be69b59902be1409879879bf7fff6288d373
Zify <cadastro@zify.com.br>
2024-06-30 15:52:29 -0300

    Aula 18

---
+++ aula-18.js
@@ -0,0 +1,14 @@
+// propriedade length (Quantidade de caracteres de uma palávra ou frase)
+
+const variavel = 'Olá, mundo'
+console.log(variavel.length)
+
+// método charAt (Retorna um caracter em uma determina posicao -)
+// Observação: posição inicia do 0
+console.log(variavel.charAt(6))
+console.log(variavel.charAt(7))
+
+// colcheter (Retorna um caracter de uma determina posicao -)
+// Observação: posição inicia do 0
+console.log(variavel[8])
+console.log(variavel[9])
